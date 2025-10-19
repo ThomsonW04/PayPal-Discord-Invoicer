02be17949e461ce285b1e169a9f2f7ed30235dbf
ThomsonW04 <tyclonie@gmail.com>
2025-10-19 21:25:41 +0100

    basic command now handles the creation of invoices

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from controllers.DataBaseController import DataBaseContoller
 from controllers.PayPalInvoiceController import PayPalInvoiceController
 from dotenv import load_dotenv
 import os
+import discord
 from discord.ext import commands
-from discord import app_commands, Intents
+from discord import app_commands, Intents, Interaction
 
 load_dotenv()
 
@@ -46,9 +47,25 @@ class PayPalHandler:
 class DiscordBot(commands.Bot):
     def __init__(self):
         super().__init__(intents=Intents.default(), help_command=None, command_prefix=None)
+        self.MAIN_GUILD = discord.Object(os.getenv("GUILD_ID"))
     
 def main():
     bot = DiscordBot()
+    paypal = PayPalHandler()
+
+    @bot.event
+    async def on_ready():
+        print(f"Bot is online as {bot.user}!")
+        await bot.tree.sync(guild=bot.MAIN_GUILD)
+
+    @bot.tree.command(name="create_invoice", description="Generate a PayPal invoice.", guild=bot.MAIN_GUILD)
+    @app_commands.describe(email="The email of the account holder to send an invoice to", item_name="Name of the item", item_price="Price of the item (e.g. 3.49, 50.00, 1.99)", note="Optional note to the customer")
+    async def create_invoice(interaction: Interaction, email: str, item_name: str, item_price: float, note: str = "Thank you for being an amazing customer!"):
+        local_tag = paypal.create_local_invoice()
+        invoice_id = paypal.create_draft_invoice(local_tag, note, email, {"name": item_name, "value": item_price})['id']
+        response = paypal.send_invoice(invoice_id)
+        await interaction.response.send_message(str(response))
+    
     bot.run(os.getenv('DISCORD_BOT_TOKEN'))
 
 if __name__ == "__main__":
